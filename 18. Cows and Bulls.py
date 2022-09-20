859401dc32ee73ed3ad78938b43a4c0e8eadecc9
JuanKmiloZ <111556574+JuanKmiloZ@users.noreply.github.com>
2022-09-20 17:22:20 -0500

    Update 18. Cows and Bulls.py

--- 18. Cows and Bulls.py
+++ 18. Cows and Bulls.py
@@ -1,7 +1,7 @@
 #Cows And Bulls   
-"""Exercise 18 (and Solution)
-Create a program that will play the “cows and bulls” game with the user. The game works like this:
+"""Create backend of APP Cows and Bulls!.
 
+The game works like this:
 Randomly generate a 4-digit number. Ask the user to guess a 4-digit number.
 For every digit that the user guessed correctly in the correct place, they have a “cow”. 
 For every digit the user guessed correctly in the wrong place is a “bull.” Every time the user makes a guess,
